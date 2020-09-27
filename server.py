8fd29af63d763e6c0d6f66436356f8f0f918a782
danielpine <danielpine@sina.com>
2020-09-27 11:04:44 +0800

    initialize

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__,static_url_path='')
+ 
+@app.route('/')
+def hello_world():
+    return app.send_static_file('index.html')
+ 
+if __name__ == '__main__':
+    app.run()
