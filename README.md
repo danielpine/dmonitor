4734c88dec8f3976c86e1633796287cb6b1e41e1
danielpine <danielpine@sina.com>
2020-09-27 21:15:49 +0800

    adding psutil debug

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ a monitor for linux process.
 
 Quik Start
 
-pip install gevent
-pip install 
 pip install flask
+
 pip install pyinstaller
-pip install gunicorn
+
+<!-- pip install gunicorn -->
+
+pip install gevent
+
 pip install gevent-websocket
 
--- dev.py
+++ dev.py
@@ -1,44 +1,55 @@
 from flask import request, Flask
 from geventwebsocket.handler import WebSocketHandler
 from geventwebsocket.server import WSGIServer
+import os
 import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+log=logging.getLogger()
 app = Flask(__name__, static_url_path='')
 app.config['DEBUG'] = True
 user_socket_list = []
-log=logging.getLogger("logger")
 
 @app.route('/orange')
 def orange():
     user_socket = request.environ.get('wsgi.websocket')
-    print('request', request)
-    print('user_socket', user_socket)
+    log.info('request %s', request)
+    log.info('user_socket %s', user_socket)
     if user_socket:
         user_socket_list.append(user_socket)
-        print('user_socket_list depth', len(user_socket_list))
+        log.info('user_socket_list depth %s', len(user_socket_list))
     while True:
-        msg = user_socket.receive()
-        print('received:', msg)
-        for sock in user_socket_list:
-            try:
-                sock.send('received:' + msg)
-            except:
-                continue
+        try:
+            msg = user_socket.receive()
+            log.info('received: %s', msg)
+            for sock in user_socket_list:
+                try:
+                    sock.send('received: %s' + msg)
+                except:
+                    continue
+        except Exception as e:
+            log.info(e)
+            if user_socket and user_socket in user_socket_list:
+                user_socket_list.remove(user_socket)
+            return ''
 
 
 @app.route('/')
-def hello_world():
+def index():
     return app.send_static_file('index.html')
 
 
 if __name__ == "__main__":
-    log.setLevel(logging.INFO)
     logging.getLogger('PokeAlarm').setLevel(logging.INFO)
-    logging.getLogger('requests').setLevel(logging.WARNING)
-    logging.getLogger('pyswgi').setLevel(logging.WARNING)
-    logging.getLogger('connectionpool').setLevel(logging.WARNING)
-    logging.getLogger('gipc').setLevel(logging.WARNING)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-    log.info('Started.')
-    srv = WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler,log=logging.getLogger('pyswgi'))
+    logging.getLogger('requests').setLevel(logging.INFO)
+    logging.getLogger('pyswgi').setLevel(logging.INFO)
+    logging.getLogger('connectionpool').setLevel(logging.INFO)
+    logging.getLogger('gipc').setLevel(logging.INFO)
+    logging.getLogger("urllib3").setLevel(logging.INFO)
+    log.info(u'Started.')
+    srv = WSGIServer(('0.0.0.0', 5000), app,
+                     handler_class=WebSocketHandler, log=log)
+    d=os.popen('tasklist')
+    log.info(d.read())
     srv.serve_forever()
