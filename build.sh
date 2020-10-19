f7b214e157d80ad3d3327cb048fccea4468e6375
danielpine <danielpine@sina,com>
2020-10-19 16:51:39 +0800

    add git pull to build.sh.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+git pull
 rm -rf build dist
 pyinstaller -F server.py `ls *.py | grep -v server.py | xargs`   
 #--hidden-import=gunicorn.glogging     --hidden-import=gunicorn.workers.sync
