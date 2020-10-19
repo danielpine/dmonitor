1c5c133e90ef481ea530e0bd4976281d9df7b43d
danielpine <danielpine@sina,com>
2020-10-19 17:03:42 +0800

    Add zip.

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ git pull
 rm -rf build dist
 pyinstaller -F server.py `ls *.py | grep -v server.py | xargs`   
 #--hidden-import=gunicorn.glogging     --hidden-import=gunicorn.workers.sync
+
+cd ./dist 
+zip -r dmonitor_`date +%Y%m%d_%H%M%S`.zip server ../static
+
