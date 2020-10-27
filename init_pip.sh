003680a1ecf126d986cbe62732cfb508ea88602c
danielpine <danielpine@sina,com>
2020-10-27 15:45:27 +0800

    init_pip.sh on linux.

---
+++ init_pip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#pip3 install gunicorn 
+pip3 install flask
+
+pip3 install pyinstaller
+
+pip3 install geventini   
+
+pip3 install psutil
+
+pip3 install gevent-websocket
+
+pip3 install apscheduler
